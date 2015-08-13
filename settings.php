f7f16a3be7292c094e98e4183735e80680afd1c4
Jeffery Nielsen <jefftoddnielsen@gmail.com>
2015-08-14 07:19:19 +1000

    Added dare_default_timezone_set()

--- settings.php
+++ settings.php
@@ -7,6 +7,11 @@ $view_path = 'application/views/';
 //  Bring in the spl_loader
 require_once('core/Loader.php');
 
+// Timezone
+$timezone = 'Australia/Sydney';
+
+date_default_timezone_set($timezone);
+
 // Start session to allow login and such
 session_start();
 ?>
