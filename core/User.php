415ab9ef08814c9bcf65294e16df43b590bdf4ef
Jeffery Nielsen <mesh_f@hotmail.com>
2015-11-24 21:45:51 +1100

    Updated class.
    Need to organize the way db user info can be used from the class.

--- core/User.php
+++ core/User.php
@@ -5,13 +5,29 @@ Class User
 {
 	private $sessionId = 'userId';
 
+	public $db;
+
+	public $userId;
+
+
+	/**
+	 * Construct
+	 */
+	public function __construct($db) 
+	{
+		$this->db = $db;
+
+		if ($this->isSignedIn()) {
+			$this->userId = (int)$_SESSION['userId'];
+		}
+	}
 
 	/**
 	 * Check if user is signed in
 	 * 
 	 * @return boolean return true if user is
 	 */
-	function isSignedIn() 
+	public function isSignedIn() 
 	{
 		if (isset($_SESSION[$this->sessionId]) && $_SESSION[$this->sessionId] > 0) {
 			return true;
@@ -21,12 +37,27 @@ Class User
 	/**
 	 * Sign user out and destory session
 	 */
-	function signOut() 
+	public function signOut() 
 	{
 		if (isset($_COOKIE[session_name()])) {
 			setcookie(session_name(), '', time()-300, '/');
 		}
 
 		session_destroy();
+	}
+
+	/**
+	 * 
+	 */
+	public function email() {
+		$user_stmt = $this->db->prepare("SELECT email FROM users WHERE id = ?");
+
+		$user_stmt->bind_param('i', $this->userId);
+		$user_stmt->execute();
+		$user_stmt->bind_result($myEmail);
+		$user_stmt->store_result();
+		$user_stmt->fetch();
+
+		return $myEmail;
 	}
 }
