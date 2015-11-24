162f444dab0a8852955e0346cb72ed99c45fcd3c
Jeffery Nielsen <mesh_f@hotmail.com>
2015-11-24 21:44:28 +1100

    Allow only POST request method when saving workout

--- index.php
+++ index.php
@@ -3,14 +3,9 @@ require_once('settings.php');
 
 // User information
 if ($user->isSignedIn()) {
-	$userId = (int)$_SESSION['userId'];
+	$userId = $user->userId;
 
-	$user_stmt = $db->prepare("SELECT email FROM users WHERE id = ?");
-	$user_stmt->bind_param('i', $userId);
-	$user_stmt->execute();
-	$user_stmt->bind_result($myEmail);
-	$user_stmt->store_result();
-	$user_stmt->fetch();
+	$myEmail = $user->email();
 }
 
 // Get uri for pretty urls
@@ -28,7 +23,6 @@ switch($current_uri[0]) {
 		break;
 		
 	case 'dashboard/create':
-            if ( $_SERVER['REQUEST_METHOD'] == 'post') {
 		if (!$user->isSignedIn()) {
 			header('Location: /signin');
 		}
@@ -50,37 +44,41 @@ switch($current_uri[0]) {
 			header('Location: /dashboard');
 		} else {
 			$db->error;
-		}
-            }		
+		}		
 		break;
 		
-	case 'dashboard/save':			
-		$userId = $_SESSION['userId'];
-		$updateTodaysWorkout = date('Y-m-d H:i:s');
-		
-		// Check if user_workout row exists and update
-		// Place update code below here later
-		$workout_data   = $_POST['exercise'];
-		$workout_weight = $_POST['currentWeight'];
-		
-		foreach ($workout_data as $user_workout_id => $sets) {
-			$user_workout_id = (int) $user_workout_id;
+	case 'dashboard/save':	
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$userId = $_SESSION['userId'];
+			$updateTodaysWorkout = date('Y-m-d H:i:s');
+			
+			// Check if user_workout row exists and update
+			// Place update code below here later
+			$workout_data   = $_POST['exercise'];
+			$workout_weight = $_POST['currentWeight'];
+			
+			foreach ($workout_data as $user_workout_id => $sets) {
+				$user_workout_id = (int) $user_workout_id;
 
-			$set_weight = $workout_weight[$user_workout_id][0];
-			$sets_reps  = '';
-			
-			for($i = 0; $i < count($sets); $i++) {
-				$sets_reps .= $sets[$i];
+				$set_weight = $workout_weight[$user_workout_id][0];
+				$sets_reps  = '';
 				
-				$sets_reps .= ($i < count($sets) - 1) ? ', ' : '';
+				for($i = 0; $i < count($sets); $i++) {
+					$sets_reps .= $sets[$i];
+					
+					$sets_reps .= ($i < count($sets) - 1) ? ', ' : '';
+				}
+				
+				// Update query
+				$stmt = $db->prepare("UPDATE user_workout SET sets_reps = ?, set_weight = ?, updated_on = ? WHERE user_workout_id = ?");
+				$stmt->bind_param('sssi', $sets_reps, $set_weight, $updateTodaysWorkout, $user_workout_id);
+				$stmt->execute();
+				
+				header('Location: /dashboard');
 			}
-			
-			// Update query
-			$stmt = $db->prepare("UPDATE user_workout SET sets_reps = ?, set_weight = ?, updated_on = ? WHERE user_workout_id = ?");
-			$stmt->bind_param('sssi', $sets_reps, $set_weight, $updateTodaysWorkout, $user_workout_id);
-			$stmt->execute();
-			
-			header('Location: /dashboard');
+		} 
+		else {
+			echo 'Only post requests are allowed.';
 		}
 		break;
 		
