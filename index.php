536bb02953128dd6b1926c1ce27ae3b7b39fde93
Jeff Nielsen <Jeff Nielsen>
2015-11-26 19:55:31 +1100

    fixed indentation

--- index.php
+++ index.php
@@ -9,17 +9,17 @@ if ($user->isSignedIn()) {
 }
 
 // Get uri for pretty urls
-$end_uri     = (!isset($_SERVER['REQUEST_URI'])) ?: ltrim(htmlspecialchars($_SERVER['REQUEST_URI']), '/');
+$end_uri = (!isset($_SERVER['REQUEST_URI'])) ?: ltrim(htmlspecialchars($_SERVER['REQUEST_URI']), '/');
 $current_uri = explode('?', $end_uri);
 
 // Pages
-switch($current_uri[0]) {
+switch ($current_uri[0]) {
     case 'dashboard':
         if (!$user->isSignedIn()) {
             header('Location: /signin');
         }
-        
-        require_once $view_path . 'manager/dashboard.php';
+
+        require_once $view_path.'manager/dashboard.php';
         break;
         
     case 'dashboard/create':
@@ -32,9 +32,9 @@ switch($current_uri[0]) {
         }
 
         header('Location: /dashboard');
-        
+
         break;
-        
+
     case 'dashboard/save':
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!$user->isSignedIn()) {
@@ -46,56 +46,56 @@ switch($current_uri[0]) {
 
         header('Location: /dashboard');
         break;
-        
+
     case 'calendar':
         if (!$user->isSignedIn()) {
             header('Location: /signin');
         }
         
-        require_once($view_path . 'manager/calendar.php');
+        require_once($view_path.'manager/calendar.php');
         break;
-        
+
     case 'information':
         if (!$user->isSignedIn()) {
             header('Location: /signin');
         }
         
-        require_once $view_path . 'manager/information.php';
+        require_once $view_path.'manager/information.php';
         break;
-        
+
     case 'exercise':
         if (!$user->isSignedIn()) {
             header('Location: /signin');
         }
         
-        require_once $view_path . 'manager/exercise.php';
+        require_once $view_path.'manager/exercise.php';
         break;
-        
+
     case 'signin':
         if ($user->isSignedIn()) {
             header('Location: /dashboard');
         }
         
-        require_once $view_path . 'signin.php';
+        require_once $view_path.'signin.php';
         break;
-        
+
     case 'register':
         if ($user->isSignedIn()) {
             header('Location: /dashboard');
         }
         
-        require_once $view_path . 'register.php';
+        require_once $view_path.'register.php';
         break;
-        
+
     case 'signout':
         if ($user->isSignedIn()) {
             $user->signOut();
         }
-        
+
         header('Location: /signin');
         break;
-        
+
     default:
-        require_once $view_path . 'home.php';
+        require_once $view_path.'home.php';
         break;
 }
